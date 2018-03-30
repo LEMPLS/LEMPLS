4e824467dfbeb67accd1397a17857690f5139f64
Martin Kapal <flamecze@gmail.com>
2018-03-30 19:24:59 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="LEMPLS Racing is a simracing team.">
+    <title>LEMPLS</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:700,400" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+  <div class="wrapper">
+      <div id="logo-url-wrap">
+          <div id="logo-url">
+              <img id="logo" src="img/lempls-logo.png" alt="LEMPLS">
+          </div>
+      </div>
+      <ul id="social">
+          <li>
+              <a href="https://www.facebook.com/lempls">
+                  <img class="icon" src="img/social-facebook.png" alt="Follow LEMPLS on Facebook">
+                  <br>@lempls
+              </a>
+          </li>
+          <li>
+              <a href="https://twitter.com/lemplsracing">
+                  <img class="icon" src="img/social-twitter.png" alt="Follow LEMPLS on Twitter">
+                  <br>@lemplsracing
+              </a>
+          </li>
+          <li>
+              <a href="https://www.youtube.com/channel/UCJ-yyUgW-O93CIWsAAsB7yA">
+                  <img class="icon" src="img/social-youtube.png" alt="Follow LEMPLS on YouTube">
+                  <br>LEMPLS
+              </a>
+          </li>
+      </ul>
+  </div>
+  <div id="page-footer-wrap">
+      <footer id="page-footer">
+          &copy; <?php echo date('Y'); ?> LEMPLS<br>
+      </footer>
+  </div>
+</body>
+</html>
